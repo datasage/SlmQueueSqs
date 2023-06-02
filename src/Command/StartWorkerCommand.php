6b74b0c129a4bbb40c71be954f61c41d0da24301
Dan Johansson <djohansson@tnsinc.com>
2023-06-01 22:00:18 -0500

    Add options to start command.

--- src/Command/StartWorkerCommand.php
+++ src/Command/StartWorkerCommand.php
@@ -2,7 +2,11 @@
 
 namespace SlmQueueSqs\Command;
 
+use SlmQueue\Controller\Exception\WorkerProcessException;
+use SlmQueue\Exception\ExceptionInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class StartWorkerCommand extends \SlmQueue\Command\StartWorkerCommand {
     protected function configure(): void
@@ -11,5 +15,34 @@ class StartWorkerCommand extends \SlmQueue\Command\StartWorkerCommand {
 
         $this->addOption('visibilityTimeout',  null, InputArgument::OPTIONAL);
         $this->addOption('waitTime', null, InputArgument::OPTIONAL);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $queueName = $input->getArgument('queue');
+        $queue = $this->queuePluginManager->get($queueName);
+        $worker = $this->workerPluginManager->get($queue->getWorkerName());
+
+        try {
+            $messages = $worker->processQueue($queue, $input->getArguments() + $input->getOptions());
+        } catch (ExceptionInterface $e) {
+            throw new WorkerProcessException(
+                'Caught exception while processing queue',
+                $e->getCode(),
+                $e
+            );
+        }
+
+        $messages = implode("\n", array_map(function (string $message): string {
+            return sprintf(' - %s', $message);
+        }, $messages));
+
+        $output->writeln(sprintf(
+            "Finished worker for queue '%s':\n%s\n",
+            $queueName,
+            $messages
+        ));
+
+        return 0;
     }
 }
