26ae45e63d8ca3b7d0cca5cd6d5d1fd6cc1c8dfc
Dan Johansson <datasage@theworldisgrey.com>
2025-10-19 11:45:33 -0500

    Update CI workflow to use latest actions, simplify caching, and remove redundant test bootstrap configuration

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -24,10 +24,3 @@ if  (!($loader = @include __DIR__ . '/../vendor/autoload.php')
 ) {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }
-
-/* @var $loader \Composer\Autoload\ClassLoader */
-$loader->add('SlmQueueSqsTest\\', __DIR__);
-
-if (!$config = @include __DIR__ . '/TestConfiguration.php') {
-    $config = include __DIR__ . '/TestConfiguration.php.dist';
-}
