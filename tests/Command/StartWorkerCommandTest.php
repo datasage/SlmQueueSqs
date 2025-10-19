3cce2ee574662f3ba0ea8c05fc7dbce2ed9dcd72
Dan Johansson <datasage@theworldisgrey.com>
2025-10-19 11:02:36 -0500

    Update StartWorkerCommandTest to include AWS credentials in SDK mock setup

--- tests/Command/StartWorkerCommandTest.php
+++ tests/Command/StartWorkerCommandTest.php
@@ -33,7 +33,7 @@ class StartWorkerCommandTest extends TestCase
         $this->mockHandler = new MockHandler();
 
         $serviceManager->setAllowOverride(true);
-        $serviceManager->setService(Sdk::class, new Sdk(['region' => 'us-west-1', 'handler' => $this->mockHandler]));
+        $serviceManager->setService(Sdk::class, new Sdk(['region' => 'us-west-1', 'credentials' => [ 'key' => 'my-key', 'secret' => 'my-secret' ], 'handler' => $this->mockHandler]));
         $serviceManager->setAllowOverride(false);
 
         $this->queuePluginManager = $serviceManager->get(QueuePluginManager::class);
